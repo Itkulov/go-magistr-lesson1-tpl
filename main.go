b4aca1cfaf80be9c3e3e3aea213171bc26f1ff93
Itkulov <itkulov199@mail.ru>
2025-11-23 16:55:10 +0300

    homework 1

--- main.go
+++ main.go
@@ -1,59 +1,80 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"net/http"
-	"strconv"
-	"strings"
-	"time"
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "strconv"
+    "strings"
+    "time"
 )
 
 func main() {
-	client := &http.Client{Timeout: 5 * time.Second}
-	ticker := time.NewTicker(10 * time.Second)
-	defer ticker.Stop()
+    for {
+        // Опрос сервера
+        resp, err := http.Get("http://srv.msk01.gigacorp.local/_stats")
+        if err != nil {
+            fmt.Printf("Error: %v\n", err)
+            time.Sleep(5 * time.Second)
+            continue
+        }
 
-	for range ticker.C {
-		resp, err := client.Get("http://srv.msk01.gigacorp.local/_stats")
-		if err != nil {
-			fmt.Printf("Error fetching stats: %v\n", err)
-			continue
-		}
-		body, err := io.ReadAll(resp.Body)
-		resp.Body.Close()
-		if err != nil {
-			fmt.Printf("Error reading response: %v\n", err)
-			continue
-		}
-		values := strings.Split(strings.TrimSpace(string(body)), ",")
-		if len(values) < 7 {
-			fmt.Printf("Invalid response format\n")
-			continue
-		}
+        body, err := ioutil.ReadAll(resp.Body)
+        resp.Body.Close()
+        if err != nil {
+            fmt.Printf("Error reading response: %v\n", err)
+            time.Sleep(5 * time.Second)
+            continue
+        }
 
-		loadAvg, _ := strconv.ParseFloat(values[0], 64)
-		netBytes, _ := strconv.ParseFloat(values[1], 64)
-		memTotal, _ := strconv.ParseFloat(values[2], 64)
-		memFree, _ := strconv.ParseFloat(values[4], 64)
-		diskFree, _ := strconv.ParseFloat(values[6], 64)
+        // Парсинг данных
+        values := strings.Split(strings.TrimSpace(string(body)), ",")
+        if len(values) != 7 {
+            fmt.Printf("Invalid data format\n")
+            time.Sleep(5 * time.Second)
+            continue
+        }
 
-		memUsed := memTotal - memFree
-		memUsage := float64(memUsed) / float64(memTotal) * 100
-		diskFreeMb := diskFree / (1024 * 1024)
-		availableNetMbit := netBytes * 8 / 1000000
+        // Парсинг значений
+        loadAvg, _ := strconv.ParseFloat(values[0], 64)
+        totalMem, _ := strconv.ParseUint(values[1], 10, 64)
+        usedMem, _ := strconv.ParseUint(values[2], 10, 64)
+        totalDisk, _ := strconv.ParseUint(values[3], 10, 64)
+        usedDisk, _ := strconv.ParseUint(values[4], 10, 64)
+        totalNet, _ := strconv.ParseUint(values[5], 10, 64)
+        usedNet, _ := strconv.ParseUint(values[6], 10, 64)
 
-		if memUsage > 80 {
-			fmt.Printf("Memory usage too high: %d%%\n", int(memUsage+0.5))
-		}
-		if diskFreeMb < 32000 {
-			fmt.Printf("Free disk space is too low: %d Mb left\n", int(diskFreeMb+0.5))
-		}
-		if availableNetMbit < 20 {
-			fmt.Printf("Network bandwidth usage high: %d Mbit/s available\n", int(availableNetMbit+0.5))
-		}
-		if loadAvg > 30 {
-			fmt.Printf("Load Average is too high: %.0f\n", loadAvg)
-		}
-	}
+        // Проверка порогов и вывод сообщений
+        messages := []string{}
+
+        // Load Average
+        if loadAvg > 15 {
+            messages = append(messages, fmt.Sprintf("Load Average is too high: %.0f", loadAvg))
+        }
+
+        // Memory usage
+        memoryUsage := float64(usedMem) / float64(totalMem) * 100
+        if memoryUsage > 90 {
+            messages = append(messages, fmt.Sprintf("Memory usage too high: %.0f%%", memoryUsage))
+        }
+
+        // Disk space
+        freeDiskMB := (totalDisk - usedDisk) / (1024 * 1024)
+        if freeDiskMB < 25000 {
+            messages = append(messages, fmt.Sprintf("Free disk space is too low: %d Mb left", freeDiskMB))
+        }
+
+        // Network bandwidth
+        availableNetMbit := (totalNet - usedNet) / 1000000
+        if availableNetMbit < 1000 {
+            messages = append(messages, fmt.Sprintf("Network bandwidth usage high: %d Mbit/s available", availableNetMbit))
+        }
+
+        // Вывод всех сообщений
+        for _, msg := range messages {
+            fmt.Println(msg)
+        }
+
+        time.Sleep(5 * time.Second)
+    }
 }
