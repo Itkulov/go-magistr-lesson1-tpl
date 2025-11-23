2f07541bed309ccbe80daf69530a3bd52729afde
Itkulov <itkulov199@mail.ru>
2025-11-23 16:51:42 +0300

    homework 1

--- main.go
+++ main.go
@@ -36,24 +36,24 @@ func main() {
 		netBytes, _ := strconv.ParseFloat(values[1], 64)
 		memTotal, _ := strconv.ParseFloat(values[2], 64)
 		memFree, _ := strconv.ParseFloat(values[4], 64)
-		diskFreeBytes, _ := strconv.ParseFloat(values[6], 64)
+		diskFree, _ := strconv.ParseFloat(values[6], 64)
 
 		memUsed := memTotal - memFree
-		memPercent := memUsed / memTotal * 100
-		diskFreeMB := diskFreeBytes / (1024 * 1024)
-		netMbit := netBytes * 8 / 1e6 // байты в мегабиты
+		memUsage := float64(memUsed) / float64(memTotal) * 100
+		diskFreeMb := diskFree / (1024 * 1024)
+		availableNetMbit := netBytes * 8 / 1000000
 
+		if memUsage > 80 {
+			fmt.Printf("Memory usage too high: %d%%\n", int(memUsage+0.5))
+		}
+		if diskFreeMb < 32000 {
+			fmt.Printf("Free disk space is too low: %d Mb left\n", int(diskFreeMb+0.5))
+		}
+		if availableNetMbit < 20 {
+			fmt.Printf("Network bandwidth usage high: %d Mbit/s available\n", int(availableNetMbit+0.5))
+		}
 		if loadAvg > 30 {
 			fmt.Printf("Load Average is too high: %.0f\n", loadAvg)
-		}
-		if memPercent > 80 {
-			fmt.Printf("Memory usage too high: %d%%\n", int(memPercent))
-		}
-		if diskFreeMB < 35000 {
-			fmt.Printf("Free disk space is too low: %d Mb left\n", int(diskFreeMB))
-		}
-		if netMbit > 15 {
-			fmt.Printf("Network bandwidth usage high: %d Mbit/s available\n", int(netMbit))
 		}
 	}
 }
